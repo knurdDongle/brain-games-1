8cfd1d92e71258a70290fcdd97ffa1c881d37c2b
andrey <Andrey>
2018-02-13 16:57:30 +0300

    add gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,19 @@
+import { random, pair } from '../utils';
+import gameFlow from './game-flow';
+
+const gcd = (a, b) => {
+  if (a === b) {
+    return a;
+  }
+  return a > b ? gcd(a - b, b) : gcd(a, b - a);
+};
+
+const taskMessage = 'Find the greatest common divisor of given numbers.';
+const game = () => {
+  const num1 = random(1, 101);
+  const num2 = random(1, 101);
+
+  return pair(`${num1} ${num2}`, String(gcd(num1, num2)));
+};
+
+export default () => gameFlow(taskMessage, game);
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const random = (min = 0, max = 100) => Math.floor(Math.random() * (max - min)) + min;
+export const random = (min = 0, max = 101) => Math.floor(Math.random() * (max - min)) + min;
 
 export const pair = (first, second) => select => (select === 'first' ? first : second);
 export const first = f => f('first');
